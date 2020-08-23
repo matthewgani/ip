3b9830f45afc99d2dafbd62849ad9250d3bde8cb
matthewgani <matthewgani2@gmail.com>
2020-08-24 00:04:06 +0800

    Completed Duke's Level-1: Greet, Echo, Exit. Cleaned up code a bit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,54 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+
+    public static void printWelcome() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("______________________________");
-        System.out.println("Hello! I'm Duke");
+
+        printDividerLine();
+        System.out.println("Hello! I'm Duke \n " + logo);
         System.out.println("What can I do for you?");
-        System.out.println("______________________________");
+        printDividerLine();
+
+
+    }
+    public static void printDividerLine() {
+        System.out.println("___________________________________ \n");
+    }
+    public static void printGoodbye() {
+        printDividerLine();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________");
+        printDividerLine();
+    }
+    public static void main(String[] args) {
+        Scanner userResponseScanner = new Scanner(System.in);
+        boolean quitResponseLoop = false;
+        printWelcome();
+        String userResponse;
+        while(!quitResponseLoop) {
+            userResponse = userResponseScanner.nextLine();
+            if(!userResponse.equals("bye")) {
+                printDividerLine();
+                System.out.println(userResponse);
+                printDividerLine();
+
+            }
+            else {
+                printGoodbye();
+                quitResponseLoop = true;
+            }
+
+        }
+
+
+
+
+
 
     }
 }
